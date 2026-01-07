b68356f38c4630c75a92c3a328247af8e3bbcf22
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-01-07 12:14:00 +0530

    fixed variable name

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 $host = "db";
-$dbname = "db";
+$db = "db";
 $user = "db";
 $pass = "db";
 
-$pdo = new PDO("mysql:host=$host;db=$dbname", $user, $pass);
+$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
