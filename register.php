efe90fb9aa2f9f6da4aab834a72019da146ef609
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-01-07 20:28:10 +0530

    registration page

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <h1>Register</h1>
+    <p>Create your account, it only takes a minute</p>
+    <form action="process.php" method="POST">
+        <label>Username:</label>
+        <input type='text' name='username' ><br><br>
+        <!-- <label>Email:</label>
+        <input type='text' name='email' required><br><br> -->
+        <label>Password:</label>
+        <input type='text' name='password' ><br><br>
+        
+        <button type="submit">Register Now</button>
+    
+</body>
+</html>
