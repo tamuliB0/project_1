42bb9c9ac2fd0458591cb5eb17312ce016969cfb
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-01-08 19:12:18 +0530

    header section add

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_title; ?></title>
+</head>
+<p style = "text-align: center">HEADER area <hr>
+<body>
