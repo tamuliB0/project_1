23a2d98f11c2c16a195edccb2445570fcbe592da
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-01-08 19:13:17 +0530

    updated with header-footer

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+$page_title = "page";
+include 'header.php';
 
 echo "<h1>Welcome to php</h1>";
+include 'footer.php';
--- success.php
+++ success.php
@@ -1,23 +1,16 @@
 <?php
 session_start();
-
+$page_title = "Page";
+include 'header.php';
 if(!isset($_SESSION['username'])) {
     header('Location: login.php');
     exit;
 }
-
 $username = $_SESSION['username'];
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page</title>
-</head>
-<body>
+
     <h2>Logged in successfully!</h2>
     <p>Welcome back,<?php echo $username; ?></p>
     <button><a href="login.php">Go back</button>
-</body>
-</html>
+
+<?php include 'footer.php'; ?>
