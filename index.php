febadfe3727402d001d1ea9e2948375309a14f33
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-01-09 15:03:57 +0530

    homepage add

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-$page_title = "page";
+session_start();
+$page_title = "Quiz app";
+// $page_header = "General Knowledge Quiz";
 include 'header.php';
+require 'questions.php';
+?>
 
-echo "<h1>Welcome to php</h1>";
-include 'footer.php';
+<h1>General Knowledge Quiz</h1>
+<p>Test your knowledge with <?php echo $total_question?> questions!</p><br>
+<a href="questions.php">Start Quiz</a>
+
+<?php include 'footer.php'; ?>
